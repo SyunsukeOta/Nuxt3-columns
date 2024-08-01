e0e8573d6726714006790a475d9cced2684bbb88
kibaota <ohta.syunsuke@liveout.co.jp>
2024-08-01 11:42:47 +0900

    ステージの作成と、ブロックの移動の実装

--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,27 @@ import { jewelList } from "@/jewelData"
 
 const pixiContainer = ref<HTMLDivElement | null>(null)
 
-const jewelSize = ref(100)
+const jewelSize = ref(20)
 const jewelMax = ref(3)
 const jewelTop = ref(50)
 const jewelLeft = ref(50)
+const boardCellHeight = ref(13)
+const boardCellWidth = ref(6)
+const startTopId = ref(0)
+const startLeftId = ref(2)
+const boardColor = ref(0x999999)
 
 const block = ref<BlockType[]>([])
+const blockTopId = ref(startTopId.value)
+const blockLeftId = ref(startLeftId.value)
 
 const handleClick = () => {
-	rotateBlock(block.value as BlockType[])
+	rotateBlock()
 	console.log("-----");
-	consoleBlock(block.value as BlockType[])
+	consoleBlock()
 }
 
-const makeJewel = (app: Application, drawLeft: Ref<number>, drawTop: Ref<number>, jewelIndex: number): BlockType => {
+const makeJewel = (app: Application, jewelIndex: number): BlockType => {
 	const jewelColor = jewelList[Math.floor(Math.random()*jewelList.length)]
 	const jewel: Graphics = new Graphics()
 		.rect(0, 0, jewelSize.value, jewelSize.value)
@@ -26,34 +33,89 @@ const makeJewel = (app: Application, drawLeft: Ref<number>, drawTop: Ref<number>
 
 	app.stage.addChild(jewel)
 
-	jewel.x = drawLeft.value
-	jewel.y = drawTop.value + jewelIndex*jewelSize.value
+	jewel.x = jewelLeft.value + startLeftId.value*jewelSize.value
+	jewel.y = jewelTop.value + (startTopId.value + jewelIndex)*jewelSize.value
 	console.log(jewelColor);
 	
 	return { jewel, jewelColor }
 }
 
-const rotateBlock = (block: BlockType[]) => {
-	let tailJewel = block.pop()
-	if (tailJewel != undefined) {
-		block.unshift(tailJewel)
-	}
-	for (const [index, jewel] of block.entries()) {
-		jewel.jewel.y = jewelTop.value + index*jewelSize.value
+const setPlace = () => {
+	for (const [index, jewel] of block.value.entries()) {
+		jewel.jewel.x = jewelLeft.value + blockLeftId.value*jewelSize.value
+		jewel.jewel.y = jewelTop.value + (blockTopId.value + index)*jewelSize.value
 	}
 }
 
-const consoleBlock = (block: BlockType[]) => {
-	if (block.length) {
-		for (const [index, jewel] of block.entries()) {
+const rotateBlock = () => {
+	let tailJewel = block.value.pop()
+	if (tailJewel != undefined) {
+		block.value.unshift(tailJewel)
+	}
+	setPlace()
+}
+
+const consoleBlock = () => {
+	if (block.value.length) {
+		for (const [index, jewel] of block.value.entries()) {
 			console.log(`index: ${index}, jewelColor: ${jewel.jewelColor.name}`);
 		}
 	}
 }
 
+const moveLeft = () => {
+	if (blockLeftId.value > 0) {
+		console.log('Left OK');
+		blockLeftId.value--
+		setPlace()
+	}
+}
+
+const moveRight = () => {
+	if (blockLeftId.value < boardCellWidth.value - 1) {
+		console.log(('Right OK'));
+		blockLeftId.value++
+		setPlace()
+	}
+}
+
+const moveDown = () => {
+	if (blockTopId.value < boardCellHeight.value - jewelMax.value) {
+		console.log('Down OK');
+		blockTopId.value++
+		setPlace()
+	}
+}
+
+const keyAction = (event: KeyboardEvent) => {
+	if (event.key.startsWith('Arrow')) {
+		event.preventDefault()
+		switch (event.key) {
+			case 'ArrowLeft':
+				moveLeft()
+				break;
+			case 'ArrowRight':
+				moveRight()
+				break;
+			case 'ArrowDown':
+				moveDown()
+				break;
+			default:
+				break;
+		}
+	}
+	console.log(event.key);
+	if (event.key == ' ') {
+		event.preventDefault()
+		rotateBlock()
+		console.log("-----");
+		consoleBlock()
+	}
+}
 
 onMounted(async () => {
 	if (pixiContainer.value) {
+		window.addEventListener('keydown', keyAction)
 		const canvas = document.createElement('canvas');
 		pixiContainer.value.appendChild(canvas);
 		try {
@@ -66,13 +128,21 @@ onMounted(async () => {
 			})
 			app.stage.interactive = true
 
+			// stage
+			const board: Graphics = new Graphics()
+				.rect(0, 0, boardCellWidth.value*jewelSize.value, boardCellHeight.value*jewelSize.value)
+				.fill(boardColor.value)
+			app.stage.addChild(board)
+			board.x = jewelLeft.value
+			board.y = jewelTop.value
+
 			// jewel
 			block.value = []
 			for (let i = 0; i < jewelMax.value; i++) {
-				block.value.push(makeJewel(app, jewelTop, jewelLeft, i))
+				block.value.push(makeJewel(app, i))
 			}
 			console.log("-----");
-			consoleBlock(block.value as BlockType[])
+			consoleBlock()
 
 		} catch (error) {
 			console.error("Error initializing PixiJS:", error);
@@ -80,7 +150,9 @@ onMounted(async () => {
 	}
 });
 
-
+onBeforeUnmount(() => {
+	window.removeEventListener('keydown', keyAction)
+})
 
 </script>
 
